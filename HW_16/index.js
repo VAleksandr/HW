1b815d579f3544260311f87052780039ea73717e
VAleksandr <vlasovgomel@gmail.com>
2022-01-10 12:57:00 +0300

    added files

---
+++ HW_16/index.js
@@ -0,0 +1,49 @@
+const userName = document.getElementById('Username');
+const passWord = document.getElementById('Password');
+const btn = document.getElementById('Btn');
+const btn_reg = document.getElementById('Btn_reg');
+
+const authorizationFun = () => {
+
+  if (localStorage.getItem('user') === null) {
+    alert ('Make registration.');
+  } else {
+    users = JSON.parse(localStorage.getItem('user'));
+    users.find(({name, sign}) => name === userName.value && sign === passWord.value)?
+      window.location = 'calculator.html':
+      alert ('Error. Login or password is incorrect or make registration.'); 
+  }
+}
+
+const registration = () => {
+  let users = [];
+  const user = {
+    name: userName.value,
+    sign: passWord.value
+  };
+  
+  if (localStorage.getItem('user') === null) {
+    users.push(user);
+    localStorage.setItem('user', JSON.stringify(users));
+    alert ('Registration completed successfully.');  
+  } else {
+    users = JSON.parse(localStorage.getItem('user'));
+    console.log(users);
+
+    if (users.find(({name, sign}) => name === userName.value)) {
+      alert ('Enter another username.')
+    } else {
+      users.push(user);
+      localStorage.setItem('user', JSON.stringify(users)); 
+      alert ('Registration completed successfully.');
+    }
+  }
+}
+
+btn.onclick = () => {
+  authorizationFun();
+}
+
+btn_reg.onclick = () => {
+  registration();
+}
